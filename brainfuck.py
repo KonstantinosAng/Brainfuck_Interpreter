6abf7ed9a546826a4135049b3fae068bfc250e4e
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-03-14 20:53:35 +0200

    Added Interpreter

---
+++ brainfuck.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python
+"""
+SIMPLE BRAINF*CK INTERPRETER AS CLASS
+
+Usage:
+
+    Windows:
+
+        - For Testing Hello_World:
+                python brainfuck.py --m=test
+
+        - For passing a file:
+                python brainfuck.py filename
+
+        filename must always be *.bf
+
+    Linux:
+        Execute Command:
+                chmod +x brainfuck.py
+
+        - For Testing Hello_World:
+                ./brainfuck.py --m=test
+
+        - For passing a file:
+                ./brainfuck.py filename
+
+        filename must always be *.bf
+
+"""
+
+
+class Brainfuck:
+
+    def __init__(self, code=None):
+        if code is None:
+            import sys
+            if len(sys.argv) == 2:
+                file = sys.argv[1]
+                if file[-3:] == '.bf':
+                    with open(file, 'r') as f:
+                        self._code = f.read()
+                else:
+                    raise Exception('[ARGUMENT ERROR] Not a brainf*ck file. File must end with .bf')
+            else:
+                raise Exception(f"Usage: python {sys.argv[0]} filename")
+        else:
+            if isinstance(code, str):
+                self._code = code
+            else:
+                raise Exception(f'[ARGUMENT ERROR] Code is not type string: {code}')
+        self._buffer = [0]
+        self._pointer = 0
+        self._loop_map = []
+
+    def syntax(self):
+        brackets = 0
+        for c in self._code:
+            if c == '[' or c == ']':
+                brackets += 1
+            if c not in ['+', '-', '<', '>', '.', ',', '[', ']']:
+                raise Exception(f'[Syntax Error] Unexpected character: {c}')
+        if brackets % 2 != 0:
+            raise Exception("[Syntax Error] Loop Brackets miss match!")
+
+    def loop_mapping(self):
+        loops = []
+        for i, c in enumerate(self._code):
+            if c == '[':
+                loops.append(i)
+            if c == ']':
+                self._loop_map.append([loops[-1], i])
+                del loops[-1]
+
+    def evaluate(self):
+        import sys
+        self.syntax()
+        self.loop_mapping()
+        i = 0
+        while i < len(self._code):
+            c = self._code[i]
+            if c == '+':
+                if self._buffer[self._pointer] < 255:
+                    self._buffer[self._pointer] += 1
+                else:
+                    self._buffer[self._pointer] = 0
+            if c == '-':
+                if self._buffer[self._pointer] > 0:
+                    self._buffer[self._pointer] -= 1
+                else:
+                    self._buffer[self._pointer] = 255
+            if c == '>':
+                if len(self._buffer) <= 2 ** 16:
+                    self._buffer.append(0)
+                    self._pointer += 1
+                else:
+                    self._pointer = 0
+            if c == '<':
+                if self._pointer == 0:
+                    self._pointer = len(self._buffer) - 1
+                else:
+                    self._pointer -= 1
+            if c == '[':
+                if self._buffer[self._pointer] == 0:
+                    for s, e in self._loop_map:
+                        if s == i:
+                            i = e
+                            break
+            if c == ']':
+                if self._buffer[self._pointer] != 0:
+                    for s, e in self._loop_map:
+                        if e == i:
+                            i = s
+                            break
+            if c == '.':
+                sys.stdout.write(chr(self._buffer[self._pointer]))
+            if c == ',':
+                user_input = input("Enter Value: ")
+                if len(user_input) == 1:
+                    self._buffer[self._pointer] = ord(user_input)
+                else:
+                    raise Exception(f"[INPUT ERROR] Only one byte is allowed: {user_input}")
+            i += 1
+
+
+if __name__ == "__main__":
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option('--mode', action='store', default='run', type='string', dest='mode', help='Run Hello world')
+    (options, args) = parser.parse_args()
+
+    if options.mode == 'test':
+        interpreter = Brainfuck(code='+[-[<<[+[--->]-[<<<]]]>>>-]>-.---.>..>.<<<<-.<+.>>>>>.>.<<.<-.')
+        interpreter.evaluate()
+    else:
+        interpreter = Brainfuck()
+        interpreter.evaluate()
